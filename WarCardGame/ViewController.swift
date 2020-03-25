7b105d147f4b9552f43151e9f7a03806d789a569
shieldnoob2 <22579829+shieldnoob2@users.noreply.github.com>
2020-03-25 14:54:59 -0700

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Andrew Nguyen on 3/25/20.
+//  Copyright © 2020 Andrew Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
